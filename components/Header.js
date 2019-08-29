c7f925965d82248ee1d554f6f18a94b9808e876e
Dídac García <didaquis@gmail.com>
2019-08-29 18:53:24 +0200

    Print styles has been fixed

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
 			<title>{configuration.productName} changelog</title>
 
 			<link href="/static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+			<link href="/static/styles/print-styles.css" rel="stylesheet" />
 			<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/static/favicon.ico" />
 			<script src="/static/vendor/jquery/jquery.min.js"></script>
 			<script src="/static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
