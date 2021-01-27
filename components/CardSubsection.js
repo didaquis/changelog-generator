95e6a437ab0a3e6cc2c3b64d32fcbde36f1b8b28
Dídac García <didaquis@gmail.com>
2021-01-27 23:52:28 +0100

    Updated dependencies

--- components/CardSubsection.js
+++ components/CardSubsection.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const CardSubsection = (props) => {
 	return (
 		<React.Fragment>
--- components/GroupOfCards.js
+++ components/GroupOfCards.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import Card from './Card'
 import md5 from 'md5'
 
