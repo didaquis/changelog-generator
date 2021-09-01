f02ebd4a8c6464cf56a7433c651b00cf5bb91476
Dídac García <didaquis@gmail.com>
2021-09-01 21:38:13 +0200

    Updated to Next.js 11

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,11 @@ const Header = () => {
 			<meta name="description" content={configuration.titleOfWebsite} />
 			<title>{configuration.titleOfWebsite}</title>
 
-			<link href="/static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-			<link href="/static/styles/print-styles.css" rel="stylesheet" />
-			<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/static/favicon.ico" />
-			<script src="/static/vendor/jquery/jquery.slim.min.js"></script>
-			<script src="/static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+			<link href="/styles/print-styles.css" rel="stylesheet" />
+			<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico" />
+			<script src="/vendor/jquery/jquery.slim.min.js"></script>
+			<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 		</Head>
 	)
 }
